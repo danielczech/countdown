894464edf03338ec25a7d6d59e21147a4cdf5b92
daniel <daniel.czech@protonmail.com>
2021-04-21 18:42:30 +0200

    Attempt to retrigger animation.

--- js/countdown.js
+++ js/countdown.js
@@ -6,6 +6,7 @@ function init(){
   var now = Date.now()/1000;
   progress = (now - START_TIME)/(END_TIME - START_TIME)*CIRC;
   document.documentElement.style.setProperty('--progress', progress);
+  document.documentElement.classlist.remove("container");
+  document.documentElement.classlist.add("container");
 }
-
 
