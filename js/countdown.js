15394cf6f88f0d0d18fd36f5985a11e9969c0385
daniel <daniel.czech@protonmail.com>
2022-11-28 22:52:37 +0200

    Update and adjust limits.

--- js/countdown.js
+++ js/countdown.js
@@ -1,12 +1,13 @@
-var START_TIME = 1636445392;
-var END_TIME = 1636714800;
+var START_TIME = 1669622400;
+var END_TIME = 1669982400;
 var CIRC = 377;
 
 function init(){
   var now = Date.now()/1000;
-  progress = (now - START_TIME)/(END_TIME - START_TIME)*CIRC;
-  document.documentElement.style.setProperty('--progress', progress);
-  document.documentElement.classlist.remove("container");
-  document.documentElement.classlist.add("container");
+  if (now <= END_TIME){
+    progress = (now - START_TIME)/(END_TIME - START_TIME)*CIRC;
+    document.documentElement.style.setProperty('--progress', progress);
+    document.documentElement.classlist.remove("container");
+    document.documentElement.classlist.add("container");
+  }
 }
-
